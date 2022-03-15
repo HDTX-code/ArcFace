a8a07a1fe0ce317f288cf1b8c9459cab5d9d6caa
HDTX-code <1252900424@qq.com>
2022-03-15 16:31:02 +0800

    预测

--- predict.py
+++ predict.py
@@ -50,21 +50,22 @@ def go_predict(a, data_root_path, save_root_path, model_30_url, model_15_30_url,
         l0 = 0
         submission = pd.DataFrame(columns=['Image', 'Id'])
         for item in range(len(path_list)):
-            top4_30, top4_index_30 = get_pre(path_list[item], dict_id, dict_id_all_30, new_d_all_30, Feature_train_30,
+            path = os.path.join(os.path.join(data_root_path, "test"), path_list[item])
+            top4_30, top4_index_30 = get_pre(path, dict_id, dict_id_all_30, new_d_all_30, Feature_train_30,
                                              target_train_30, opt_30, model_30, device, float(th))
             l30 = l30 + len(top4_30)
-            top4_15_30, top4_index_15_30 = get_pre(path_list[item], dict_id, dict_id_all_15_30, new_d_all_15_30,
+            top4_15_30, top4_index_15_30 = get_pre(path, dict_id, dict_id_all_15_30, new_d_all_15_30,
                                                    Feature_train_15_30,
                                                    target_train_15_30, opt_15_30, model_15_30, device, float(th))
             l15 = l15 + len(top4_15_30)
-            top4_10_14, top4_index_10_14 = get_pre(path_list[item], dict_id, dict_id_all_10_14, new_d_all_10_14,
+            top4_10_14, top4_index_10_14 = get_pre(path, dict_id, dict_id_all_10_14, new_d_all_10_14,
                                                    Feature_train_10_14,
                                                    target_train_10_14, opt_10_14, model_10_14, device, float(th))
             l10 = l10 + len(top4_10_14)
             Top4 = np.concatenate((top4_30, top4_15_30, top4_10_14), axis=1)
             Top4_index = np.concatenate((top4_index_30, top4_index_15_30, top4_index_10_14), axis=1)
             if len(Top4) < 4:
-                top4_7_9, top4_index_7_9 = get_pre(path_list[item], dict_id, dict_id_all_7_9, new_d_all_7_9,
+                top4_7_9, top4_index_7_9 = get_pre(path, dict_id, dict_id_all_7_9, new_d_all_7_9,
                                                    Feature_train_7_9,
                                                    target_train_7_9, opt_7_9, model_7_9, device, float(th),
                                                    len(Top4) - 4)
@@ -72,7 +73,7 @@ def go_predict(a, data_root_path, save_root_path, model_30_url, model_15_30_url,
                 Top4_index = np.concatenate((Top4_index, top4_index_7_9), axis=1)
                 l7 = l7 + len(top4_7_9)
                 if len(Top4) < 4:
-                    top4_5_6, top4_index_5_6 = get_pre(path_list[item], dict_id, dict_id_all_5_6, new_d_all_5_6,
+                    top4_5_6, top4_index_5_6 = get_pre(path, dict_id, dict_id_all_5_6, new_d_all_5_6,
                                                        Feature_train_5_6,
                                                        target_train_5_6, opt_5_6, model_5_6, device, float(th),
                                                        len(Top4) - 4)
@@ -80,7 +81,7 @@ def go_predict(a, data_root_path, save_root_path, model_30_url, model_15_30_url,
                     Top4_index = np.concatenate((Top4_index, top4_index_5_6), axis=1)
                     l5 = l5 + len(top4_index_5_6)
                     if len(Top4) < 4:
-                        top4_0_4, top4_index_0_4 = get_pre(path_list[item], dict_id, dict_id_all_0_4, new_d_all_0_4,
+                        top4_0_4, top4_index_0_4 = get_pre(path, dict_id, dict_id_all_0_4, new_d_all_0_4,
                                                            Feature_train_0_4,
                                                            target_train_0_4, opt_0_4, model_0_4, device, -100,
                                                            len(Top4) - 4)
@@ -98,14 +99,14 @@ def go_predict(a, data_root_path, save_root_path, model_30_url, model_15_30_url,
 
 
 if __name__ == '__main__':
-    go_predict(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6],
-               sys.argv[7], sys.argv[8], sys.argv[9])
-    # go_predict(0, "D:\\project\\humpWhale\\data\\humpback-whale-identification",
-    #            "D:\\project\\humpWhale\\data\\humpback-whale-identification",
-    #            "D:\\edge\\gt30_model.pth",
-    #            "D:\\edge\\resnet50_15-30_loss_ 0.7051018987383161score_ 0.9230769230769231.pth",
-    #            "D:\\edge\\resnet50_10-14_loss_ 0.028108565703682278score_ 0.8936170212765957.pth",
-    #            "D:\\edge\\resnet50_7-9_loss_ 1.95732459243463score_ 0.78125.pth",
-    #            "D:\\edge\\resnet50_5-6_loss_ 2.8928167653638264score_ 0.5584415584415584.pth",
-    #            "D:\\edge\\resnet50Sph-29loss_ 8.339440341671137score_ 0.pth",
-    #            0.6)
+    # go_predict(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6],
+    #            sys.argv[7], sys.argv[8], sys.argv[9])
+    go_predict(0, "D:\\project\\humpWhale\\data\\humpback-whale-identification",
+               "D:\\project\\humpWhale\\data\\humpback-whale-identification",
+               "D:\\edge\\gt30_model.pth",
+               "D:\\edge\\resnet50_15-30_loss_ 0.7051018987383161score_ 0.9230769230769231.pth",
+               "D:\\edge\\resnet50_10-14_loss_ 0.028108565703682278score_ 0.8936170212765957.pth",
+               "D:\\edge\\resnet50_7-9_loss_ 1.95732459243463score_ 0.78125.pth",
+               "D:\\edge\\resnet50_5-6_loss_ 2.8928167653638264score_ 0.5584415584415584.pth",
+               "D:\\edge\\resnet50Sph-29loss_ 8.339440341671137score_ 0.pth",
+               0.6)
