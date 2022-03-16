e6321488cf931729d7343cbc535cdb6f0500cf44
HDTX-code <1252900424@qq.com>
2022-03-16 12:37:45 +0800

    预测

--- utils/get_pre.py
+++ utils/get_pre.py
@@ -27,7 +27,8 @@ def get_pre(test_path, dict_id, dict_id_all, new_d_all, Feature_train, target_tr
             output = F.cosine_similarity(
                 torch.mul(torch.ones(Feature_train_F.shape).to(device), feature_test[0, :].T.to(device)),
                 Feature_train_F.to(device), dim=1).to(device)
-            Score[j] = output.cpu().detach().numpy().mean()
+            score = output.cpu().detach().numpy()
+            Score[j] = sum(score) / len(sum)
         top4_index = Score.argsort()[it:]
         top4 = Score.sort()[it:]
         top4 = top4[top4 > th]
