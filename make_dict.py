406f369064fa422161d31fbefb3062b6fc382534
HDTX-code <1252900424@qq.com>
2022-03-17 14:18:47 +0800

    重新训练

--- make_dict.py
+++ make_dict.py
@@ -1,8 +1,12 @@
 from __init__ import *
 
 
-def make_dict(data_csv_path, dict_id_path):
+def make_dict(a, data_csv_path, dict_id_path):
     train_csv = pd.read_csv(data_csv_path)
     train_csv_id = train_csv['Id'].unique()
     dict_id = dict(zip(train_csv_id, range(len(train_csv_id))))
     np.save(os.path.join(dict_id_path, "dict_id"), dict_id)
+
+
+if __name__ == '__main__':
+    make_dict(sys.argv[0], sys.argv[1], sys.argv[2])
