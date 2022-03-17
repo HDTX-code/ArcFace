b60e5a99a6877a9ab363312187c61b587368aa2c
HDTX-code <1252900424@qq.com>
2022-03-17 21:15:01 +0800

    图像增强

--- utils/make_csv.py
+++ utils/make_csv.py
@@ -11,7 +11,6 @@ def make_csv(opt, dict_id_path):
     dict_id = dict(zip(train_csv_id, range(len(train_csv_id))))
     # 选取数据量大于等于 low 小于等于 high 的数据
     train_csv_describe = pd.DataFrame(columns=['Id', 'num'])
-    train_csv_train = pd.DataFrame(columns=['Image', 'Id', 'd'])
     train_csv_val = pd.DataFrame(columns=['Image', 'Id', 'd'])
     # 生成类别数量统计表
     for k, v in dict_id.items():
@@ -23,6 +22,11 @@ def make_csv(opt, dict_id_path):
     train_csv_all = train_csv.loc[train_csv['Id'].isin(train_csv_all_id), :]
     train_csv_all.index = range(len(train_csv_all))
 
+    for item in train_csv_all_id:
+        train_csv_val = pd.concat([train_csv_val, train_csv_all.loc[
+                                                  train_csv_all[train_csv_all["Id"] == item].index.tolist()[
+                                                  :opt.val_number], :]], ignore_index=True)
+    train_csv_val.loc[:, 'd'] = 0
     train_csv_all.loc[:, 'd'] = 0
 
     train_csv_all_0 = copy.copy(train_csv_all)
@@ -32,18 +36,14 @@ def make_csv(opt, dict_id_path):
         F.loc[:, 'd'] = item
         train_csv_all = pd.concat([train_csv_all, F], ignore_index=True)
 
+    train_csv_train = train_csv_all.loc[~train_csv_all.isin(train_csv_val)]
+    train_csv_train = train_csv_train.dropna(axis=0, how='any')
+
     dict_id_all = dict(zip(train_csv_all_id, range(len(train_csv_all_id))))
     info_json = json.dumps(dict_id_all, sort_keys=False, indent=4, separators=(',', ': '))
     f = open(os.path.join(dict_id_path, "dict_id"), 'w')
     f.write(info_json)
 
-    for item in train_csv_all_id:
-        train_csv_train = pd.concat([train_csv_train, train_csv_all.loc[
-                                                      train_csv_all[train_csv_all["Id"] == item].index.tolist()[
-                                                      opt.val_number:], :]], ignore_index=True)
-        train_csv_val = pd.concat([train_csv_val, train_csv_all.loc[
-                                                  train_csv_all[train_csv_all["Id"] == item].index.tolist()[
-                                                  :opt.val_number], :]], ignore_index=True)
     train_csv_train.to_csv(os.path.join(opt.checkpoints_path, "train_csv_train.csv"), index=False)
     train_csv_val.to_csv(os.path.join(opt.checkpoints_path, "train_csv_val.csv"), index=False)
     return train_csv_train, train_csv_val, dict_id_all
