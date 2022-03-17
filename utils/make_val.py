21a4235b7fc8b84a3125eab98adea47370868300
HDTX-code <1252900424@qq.com>
2022-03-17 15:04:57 +0800

    重新训练

--- utils/make_val.py
+++ utils/make_val.py
@@ -10,18 +10,16 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num):
     target_train = target_train.to(device)
     Feature_val = Feature_val.to(device)
     target_val = target_val.to(device)
-    num = torch.from_numpy(num).to(device)
-    num = num.to(device)
     with torch.no_grad():
-        with tqdm(total=len(target_val), postfix=dict, file=sys.stdout) as pbar2:
+        with tqdm(total=len(target_val), postfix=dict) as pbar2:
             Score = 0
             for item in range(len(target_val)):
                 output = F.cosine_similarity(
                     torch.mul(torch.ones(Feature_train.shape).to(device), Feature_val[item, :].T),
                     Feature_train, dim=1).to(device)
                 kind = torch.zeros(num.shape)
-                for j in range(len(num)):
-                    kind[j] = output[target_train[:, 0] == num[j]].mean()
+                for j in range(num):
+                    kind[j] = output[target_train[:, 0] == j].mean()
                 sorted, indices = torch.sort(kind, descending=True)
                 if sum(num[indices[:5].to(device)] == target_val[item]) != 0:
                     Score = Score + 1
