d959d2b59c26fe138df6071a1480c0026d0565b6
HDTX-code <1252900424@qq.com>
2022-03-22 15:42:53 +0800

    更改成Arc

--- models/metrics.py
+++ models/metrics.py
@@ -45,7 +45,7 @@ class ArcMarginProduct(nn.Module):
         # --------------------------- convert label to one-hot ---------------------------
         # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
         one_hot = torch.zeros(cosine.size(), device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        one_hot.scatter_(1, label.view(-1, 1).long().cuda(), 1)
         # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
         output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is
         # 0.4
