24bd6356c4d72450536b8d345d5076129f3b5064
HDTX-code <1252900424@qq.com>
2022-03-23 15:55:18 +0800

    设立阈值判断是否要加new

--- predict.py
+++ predict.py
@@ -43,10 +43,10 @@ def go_predict(a, data_root_path, save_path, path):
                 # Top, Top_index = get_pre(Feature_test[item, :], Feature_train, target_train, dict_id,
                 #                          dict_id_all,
                 #                          4, device)
-                Top, Top_index = get_pre_num(Feature_test[item, :], Feature_train_num, dict_id, dict_id_all, 4, device)
+                Top, Top_index = get_pre_num(Feature_test[item, :], Feature_train_num, dict_id, dict_id_all, 5, device)
                 submission.loc[submission[submission.Image == new_d_test[target_test[item, 0]]].index.tolist(), "Id"] = \
                     new_d_all[Top_index[0]] + ' ' + new_d_all[Top_index[1]] + ' ' + new_d_all[Top_index[
-                        2]] + ' ' + new_d_all[Top_index[3]] + ' ' + 'new_whale'
+                        2]] + ' ' + new_d_all[Top_index[3]] + ' ' + 'new_whale' if Top[4] < 0.75 else new_d_all[Top_index[4]]
                 pbar.update(1)
                 pbar.set_postfix(
                     **{'Top': Top[3]})
