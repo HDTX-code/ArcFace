9634f312c2578f95a21d7bc64c3fe292049771ef
HDTX-code <1252900424@qq.com>
2022-03-23 17:18:58 +0800

    更改提交方式

--- predict.py
+++ predict.py
@@ -46,7 +46,7 @@ def go_predict(a, data_root_path, save_path, path):
                 Top, Top_index = get_pre_num(Feature_test[item, :], Feature_train_num, dict_id, dict_id_all, 5, device)
                 Top[4] = 0.75
                 Top_index[4] = dict_id_all['new_whale']
-                Top_index = Top_index[np.sort(-Top)]
+                Top_index = Top_index[np.argsort(-Top)]
                 # Is_new = 'new_whale' if Top[0] < 0.75 else new_d_all[Top_index[4]]
                 submission.loc[submission[submission.Image == new_d_test[target_test[item, 0]]].index.tolist(), "Id"] = \
                     new_d_all[Top_index[0]] + ' ' + new_d_all[Top_index[1]] + ' ' + new_d_all[Top_index[
