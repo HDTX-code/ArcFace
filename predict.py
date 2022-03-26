3fd614993d2b38010ea962fe4bf39dd560d46862
HDTX-code <1252900424@qq.com>
2022-03-26 21:21:23 +0800

    同步更改

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
                 # submission.loc[submission[submission.Image == new_d_test[target_test[item, 0]]].index.tolist(), "Id"] = \
                 #     new_d_all[Top_index[0]] + ' ' + 'new_whale'+ ' ' + new_d_all[Top_index[1]] + ' ' + new_d_all[Top_index[
                 #         2]] + ' ' + new_d_all[Top_index[3]]
-                Top[4] = 0.55
+                Top[4] = 0.5
                 Top_index[4] = dict_id_all['new_whale']
                 Top_index = Top_index[np.argsort(-Top)]
                 # Is_new = 'new_whale' if Top[0] < 0.75 else new_d_all[Top_index[4]]
