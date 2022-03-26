89d4b9d1bcc8b827f1b186af7cd1784a872db5a4
HDTX-code <1252900424@qq.com>
2022-03-26 23:29:14 +0800

    适配新题

--- train.py
+++ train.py
@@ -63,11 +63,10 @@ if __name__ == '__main__':
 
     # 加载backbone,默认resnet50
     if backbone == 'EfficientNet-V2':
-        model = timm.create_model('efficientnetv2_rw_m', pretrained=pretrained)
-        model.head = torch.nn.Linear(model.head.in_features, 512)
+        model = timm.create_model('efficientnetv2_rw_m', pretrained=pretrained, num_classes=512)
     else:
         model = torchvision.models.resnet50(pretrained=pretrained)
-        model.head = torch.nn.Linear(model.head.in_features, 512)
+        model.fc = torch.nn.Linear(model.fc.in_features, 512)
     if model_path != "":
         model.load_state_dict(torch.load(model_path, map_location=device), False)
 
