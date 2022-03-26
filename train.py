6ff76cd4d34bd9e715136ddd51dc4d8d4d9a8e8c
HDTX-code <1252900424@qq.com>
2022-03-26 15:55:57 +0800

    更改代码范式

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     # -------------------------------#
     #   冻结训练
     # -------------------------------#
-    Freeze_Epoch = 12
+    Freeze_Epoch = 1
     Freeze_lr = 0.5
     Freeze_lr_step = 10
     Freeze_lr_decay = 0.95  # when val_loss increase lr = lr*lr_decay
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # -------------------------------#
     #   解冻训练
     # -------------------------------#
-    Unfreeze_Epoch = 24
+    Unfreeze_Epoch = 2
     Unfreeze_lr = 1e-1  # initial learning rate
     Unfreeze_lr_step = 10
     Unfreeze_lr_decay = 0.95  # when val_loss increase lr = lr*lr_decay
