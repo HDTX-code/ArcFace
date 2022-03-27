bdc57e2a34303e0270a5ba14874d079243c4d076
HDTX-code <1252900424@qq.com>
2022-03-27 19:20:51 +0800

    换efficentNet

--- train.py
+++ train.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     # -------------------------------#
     #   冻结训练
     # -------------------------------#
-    Freeze_Epoch = 6
-    Freeze_lr = 4e-2
+    Freeze_Epoch = 1
+    Freeze_lr = 4e-3
     Freeze_lr_step = 10
     Freeze_lr_decay = 0.95  # when val_loss increase lr = lr*lr_decay
     Freeze_weight_decay = 5e-4
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     #   解冻训练
     # -------------------------------#
     Unfreeze_Epoch = 24
-    Unfreeze_lr = 0.1  # initial learning rate
+    Unfreeze_lr = 4e-4  # initial learning rate
     Unfreeze_lr_step = 10
     Unfreeze_lr_decay = 0.95  # when val_loss increase lr = lr*lr_decay
     Unfreeze_weight_decay = 5e-4
