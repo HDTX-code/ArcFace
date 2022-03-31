72a0092b2eeab1a0464959f5f34aa7ae3088d7b6
HDTX-code <1252900424@qq.com>
2022-04-01 00:30:42 +0800

    训练粗分类器

--- utils/make_train_classes.py
+++ utils/make_train_classes.py
@@ -23,7 +23,7 @@ def fit_one_epoch_classes(model, criterion, optimizer, item, max_epoch,
             image_tensor = image_tensor.type(torch.FloatTensor).to(device)
             target_t = target_t.long().to(device)
             feature = model(image_tensor).to(device)
-            loss = criterion(feature, target_t).to(device).mean()
+            loss = criterion(feature, target_t.reshape(-1)).to(device).mean()
             Loss += loss
             optimizer.zero_grad()
             loss.backward()
