d717ba705831b22784b9cf5486dc8799187af905
HDTX-code <1252900424@qq.com>
2022-04-09 22:24:57 +0800

    predict变更

--- utils/get_pre_need.py
+++ utils/get_pre_need.py
@@ -64,7 +64,7 @@ def get_pre_need(model_path, dict_id_path, train_csv_path,  device, w, h, data_t
         if Feature_train_path is None:
             train_csv_train = pd.read_csv(train_csv_path)
             train_dataset = ArcDataset(train_csv_train, dict_id, data_train_path, w, h)
-            dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,
+            dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False,
                                     num_workers=num_workers)
             Feature_train, target_train = get_feature(model, dataloader, device, 512)
             Feature_train = Feature_train.cpu().detach().numpy()
