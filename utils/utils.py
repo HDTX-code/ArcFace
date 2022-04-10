086c6ef4544ff174da0cf83f74bd833dfd6bdf14
HDTX-code <1252900424@qq.com>
2022-04-10 12:42:56 +0800

    predict变更

--- utils/utils.py
+++ utils/utils.py
@@ -92,7 +92,7 @@ def KNN_by_iter(Feature_train, target_train, Feature_test, target_test, k, devic
         for item in range(Feature_test.shape[0]):
             K = copy.copy(k)
             while True:
-                target_train_index = target_train[Index[item, :K].astype('int32'), 0]
+                target_train_index = target_train[Index[item, :K], 0]
                 print(target_train_index)
                 if len(np.unique(target_train_index)) >= 5:
                     break
