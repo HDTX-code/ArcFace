1ca44caab7c2c0ca56bc340980b6121dbaab380e
HDTX-code <1252900424@qq.com>
2022-04-12 22:09:28 +0800

    识别blueWhale

--- utils/make_train.py
+++ utils/make_train.py
@@ -57,10 +57,8 @@ def make_train(model, metric_fc, criterion, optimizer, scheduler,
                     path_featureMap = os.path.join(save_path, "FeatureMap")
                     if not os.path.exists(path_featureMap):
                         os.mkdir(path_featureMap)
-                    Feature_train = Feature_train.cpu().detach().numpy()
-                    target_train = target_train.cpu().detach().numpy()
-                    np.save(os.path.join(path_featureMap, "Feature_train_{}.npy".format(item)), Feature_train)
-                    np.save(os.path.join(path_featureMap, "target_train_{}.npy".format(item)), target_train)
+                    np.save(os.path.join(path_featureMap, "Feature_train_{}.npy".format(item)), Feature_train.cpu().detach().numpy())
+                    np.save(os.path.join(path_featureMap, "target_train_{}.npy".format(item)), target_train.cpu().detach().numpy())
                 if val_loader is not None:
                     # 计算验证得分
                     Feature_val, target_val = get_feature(model, val_loader, device, 512)
