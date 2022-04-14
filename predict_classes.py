1ab9c0a6ca86fdec56522b8a41655634e9df0cf2
HDTX-code <1252900424@qq.com>
2022-04-14 15:45:56 +0800

    粗分类器

--- predict_classes.py
+++ predict_classes.py
@@ -4,13 +4,15 @@ if __name__ == '__main__':
     # -------------------
     # 参数设置
     # -------------------
-    model_path = r"D:\project\happyWhale\classes\粗分类\weights\resnet18_epoch_30_loss_0.033053112030029294"
-    data_test_path = r"D:\edge\archive"
-    data_path_list = [r"C:\Users\12529\Desktop\0", r"C:\Users\12529\Desktop\1"]
+    model_path = r"D:\project\happyWhale\weights\res50-softmax-224\resnet50Softmax-8loss_ 0.0017252337808410327score_ 4993.pth"
+    dict_id_path = r"D:\project\happyWhale\weights\res50-softmax-224\dict_id"
+    data_test_path = r"D:\project\happyWhale\classes\CFL\test\test_all\test_all"
+    data_path_list = [r"C:\Users\12529\Desktop\beluga", r"C:\Users\12529\Desktop\big_model",
+                      r"C:\Users\12529\Desktop\hdb"]
     # path_0 = r"C:\Users\12529\Desktop\0"
     # path_1 = r"C:\Users\12529\Desktop\1"
     # path_2 = r"C:\Users\12529\Desktop\2"
-    backbone = 'resnet18'
+    backbone = 'resnet50'
     w = 224
     h = 224
     batch_size = 256
@@ -25,11 +27,7 @@ if __name__ == '__main__':
 
     print("backbone = " + backbone)
 
-    if backbone == 'EfficientNet-V2':
-        model = timm.create_model('efficientnetv2_rw_m', pretrained=False, num_classes=3)
-    else:
-        model = torchvision.models.resnet18(pretrained=False)
-        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+    model = get_model(backbone, False, 30)
     if model_path != "":
         model.load_state_dict(torch.load(model_path, map_location=device), False)
 
@@ -43,12 +41,15 @@ if __name__ == '__main__':
         submission.loc[item, "image"] = path_list[item]
     # 建立测试集地址字典
     dict_id_test = dict(zip(path_list, range(len(path_list))))
+    new_d_test = {v: k for k, v in dict_id_test.items()}
 
     test_dataset = TestDataset(submission, dict_id_test, data_test_path, w, h)
     test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False,
                                  num_workers=num_workers)
 
     Feature_test, target_test = get_feature(model, test_dataloader, device, num_classes)
+    target_test = target_test.cpu().detach().numpy()
+    Feature_test = Feature_test.cpu().detach().numpy()
 
     for item in data_path_list:
         if not os.path.exists(item):
@@ -57,7 +58,14 @@ if __name__ == '__main__':
     with tqdm(total=target_test.shape[0], postfix=dict) as pbar:
         for item in range(target_test.shape[0]):
             Feature = Feature_test[item, :]
-            Str = path_list[item]
-            shutil.copyfile(os.path.join(data_test_path, Str),
-                            os.path.join(data_path_list[Feature.argmax()], Str))
+            Str = new_d_test[target_test[item, 0]]
+            if np.argmax(Feature) == 1 or np.argmax(Feature) == 7 or np.argmax(Feature) == 3:
+                shutil.copyfile(os.path.join(data_test_path, Str),
+                                os.path.join(data_path_list[2], Str))
+            elif np.argmax(Feature) == 4:
+                shutil.copyfile(os.path.join(data_test_path, Str),
+                                os.path.join(data_path_list[0], Str))
+            else:
+                shutil.copyfile(os.path.join(data_test_path, Str),
+                                os.path.join(data_path_list[1], Str))
             pbar.update(1)
