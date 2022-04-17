a71f61acf4e0e1a23d9feed8db56cb80088d7280
HDTX-code <1252900424@qq.com>
2022-04-17 18:01:41 +0800

    完成验证部分

--- utils/make_val.py
+++ utils/make_val.py
@@ -19,7 +19,6 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num, 
             Feature_train_num[item] = np.mean(Feature_train[target_train[:, 0] == item, :], axis=0)
         Feature_train_num = torch.from_numpy(Feature_train_num).to(device)
 
-        Score, map_1, map_2, map_3, map_4, map_5, = 0, 0, 0, 0, 0, 0
         analyse_right = pd.DataFrame(columns=['image', 'species', 'individual_id', 'score',
                                               'predictions_1', 'predictions_2', 'predictions_3', 'predictions_4',
                                               'predictions_5'])
@@ -27,6 +26,12 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num, 
                                               'predictions_1', 'predictions_2', 'predictions_3', 'predictions_4',
                                               'predictions_5'])
         with tqdm(total=len(target_val), postfix=dict) as pbar2:
+            Score = 0
+            map_1 = 0
+            map_2 = 0
+            map_3 = 0
+            map_4 = 0
+            map_5 = 0
             for item in range(len(target_val)):
                 output = F.cosine_similarity(
                     torch.mul(torch.ones(Feature_train_num.shape).to(device), Feature_val[item, :].T),
@@ -38,10 +43,8 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num, 
                 sorted, indices = torch.sort(output, descending=True)
                 sorted = sorted.cpu().detach().numpy()
                 indices = indices.cpu().detach().numpy()
-                print(int(indices[0]))
-                print(int(target_val[item, 0]))
                 if int(indices[0]) == int(target_val[item, 0]):
-                    Score = Score + 1
+                    Score += 1
                     map_1 += 1
                     score = 0
                 elif int(indices[1]) == int(target_val[item, 0]):
@@ -71,11 +74,11 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num, 
                                                                         Img_id_val[item, 0]],
                                                                     'individual_id'].values[0],
                                                                 score,
-                                                                str(sorted[0]),
-                                                                str(sorted[1]),
-                                                                str(sorted[2]),
-                                                                str(sorted[3]),
-                                                                str(sorted[4])]
+                                                                new_id_all[indices[0]] + '-' + str(sorted[0]),
+                                                                new_id_all[indices[1]] + '-' + str(sorted[1]),
+                                                                new_id_all[indices[2]] + '-' + str(sorted[2]),
+                                                                new_id_all[indices[3]] + '-' + str(sorted[3]),
+                                                                new_id_all[indices[4]] + '-' + str(sorted[4])]
                 else:
                     analyse_error.loc[len(analyse_error), :] = [new_val_id[Img_id_val[item, 0]],
                                                                 train_csv_val.loc[
@@ -94,8 +97,8 @@ def make_val(Feature_train, target_train, Feature_val, target_val, device, num, 
                 pbar2.update(1)
                 pbar2.set_postfix(**{'val_Score': (Score / (item + 1)) * 1000 // 1000,
                                      'MAP5': (MAP5 / (item + 1)) * 1000 // 1000})
-        print('val_Score: ' + str((Score / (len(target_val))) * 1000 // 1000))
-        print('MAP5: ' + str((MAP5 / (len(target_val))) * 1000 // 1000))
+            print('val_Score: ' + str((Score / (len(target_val))) * 1000 // 1000))
+            print('MAP5: ' + str((MAP5 / (len(target_val))) * 1000 // 1000))
         #             if item>100:
         #                 break
     return analyse_right, analyse_error
