2780acac0b8246adeb3ce0ac3bdf4abf755336aa
HDTX-code <1252900424@qq.com>
2022-04-17 16:40:29 +0800

    完成验证分析程序

--- utils/get_feature.py
+++ utils/get_feature.py
@@ -13,21 +13,6 @@ def get_feature(model, dataloader, device, feature_num, get_id=False):
             model.to(device)
             val = 0
             if get_id:
-                for iteration, (image_tensor, target_t) in enumerate(dataloader):
-                    image_tensor = image_tensor.type(torch.FloatTensor).to(device)
-                    feature = model(image_tensor.to(device))
-                    feature.reshape(-1, feature_num).to(device)
-                    target_t.reshape(-1, 1).to(device)
-                    if val == 0:
-                        Feature = copy.copy(feature)
-                        target = copy.copy(target_t)
-                        val = 1
-                    else:
-                        Feature = torch.cat((Feature, feature), 0)
-                        target = torch.cat((target, target_t), 0)
-                    pbar3.update(1)
-                return Feature, target
-            else:
                 for iteration, (image_tensor, target_t, img_id) in enumerate(dataloader):
                     image_tensor = image_tensor.type(torch.FloatTensor).to(device)
                     feature = model(image_tensor.to(device))
@@ -45,3 +30,19 @@ def get_feature(model, dataloader, device, feature_num, get_id=False):
                         Img_id = torch.cat((Img_id, img_id), 0)
                     pbar3.update(1)
                 return Feature, target, Img_id
+            else:
+                for iteration, (image_tensor, target_t) in enumerate(dataloader):
+                    image_tensor = image_tensor.type(torch.FloatTensor).to(device)
+                    feature = model(image_tensor.to(device))
+                    feature.reshape(-1, feature_num).to(device)
+                    target_t.reshape(-1, 1).to(device)
+                    if val == 0:
+                        Feature = copy.copy(feature)
+                        target = copy.copy(target_t)
+                        val = 1
+                    else:
+                        Feature = torch.cat((Feature, feature), 0)
+                        target = torch.cat((target, target_t), 0)
+                    pbar3.update(1)
+                return Feature, target
+
